c437ea61c4d983cf18ea1352e0876697d77a7c21
Gaurav Nanda <gaurav.nanda@wingify.com>
2018-09-13 10:46:17 +0530

    Handle blank line gracefully and commit input csv

--- index.js
+++ index.js
@@ -8,32 +8,35 @@ async function run() {
   let dataArray = data.split('\n');
 
   for (let k = 1; k < dataArray.length; k++) {
+    if (dataArray[k] === '') {
+      continue;
+    }
     let keyword = dataArray[k].replace(',', '');
     console.log('Keyword being searched: ', keyword);
     const browser = await puppeteer.launch({
-      headless: false
+      headless: true
     });
     const page = await browser.newPage();
-  
+
     await page.goto("https://www.google.co.in/");
     await page.click("#lst-ib");
     await page.keyboard.type(keyword);
     await page.keyboard.down("Enter");
     await page.waitFor(2000);
-  
+
     dataToWrite += keyword + ', ';
-  
+
     let resultsLength = await page.evaluate($el => {
       return document.querySelector($el).childNodes.length;
     }, "#search > div > div > div > div > div");
-  
+
     for (let i = 1; i <= resultsLength; i++) {
       let heading =
         "#search > div > div > div > div > div > div:nth-of-type(" + i + ") > div > div > h3 > a";
       let href = await page.evaluate($el => {
         return document.querySelector($el).href;
       }, heading);
-  
+
       dataToWrite += i + '. ' + href + '\n';
     }
     dataToWrite += ',';
